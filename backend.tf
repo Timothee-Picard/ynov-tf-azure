bf4098b83634036f8c0138a288f1573991cd04a3
timothée picard <timothee.picard@outlook.fr>
2025-04-07 10:23:42 +0200

    feat: test ci

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "ynov-web"
-    storage_account_name = "ynovtfsmontri"
+    storage_account_name = "ynovtimohtee"
     container_name       = "tfstate"
     key                  = "ynov1.tfstate"
     tenant_id            = "38e72bba-3c22-4382-9323-ac1612931297"
