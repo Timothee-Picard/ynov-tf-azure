a7bbfa02fc099b314ede4abb29ea86de9659fdb5
timothée picard <timothee.picard@outlook.fr>
2025-04-07 10:59:26 +0200

    feat: update subscription id

--- backend.tf
+++ backend.tf
@@ -5,6 +5,6 @@ terraform {
     container_name       = "tfstate"
     key                  = "ynov1.tfstate"
     tenant_id            = "38e72bba-3c22-4382-9323-ac1612931297"
-    subscription_id      = "c10b661f-7a09-44f4-b6c5-aa37c4d5263c"
+    subscription_id      = "7f90acab-3e9e-42b8-9e6c-e6966ec7426a"
   }
 }
